9bd51eaf761cec7996270a0d9c3de0db18eeb5ed
chemokita13 <chemokita13@gmail.com>
2023-05-21 16:17:13 +0200

    postPage functions finished

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,5 +1,40 @@
+import PostElement from "@/app/components/post";
+import axiosInstance from "@/constants/axiosInstance";
+import { ApiResponse, Post } from "@/types/types";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+
 function PostFeed() {
-    return <div>PostFeed</div>;
+    // Posts state
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    const getPosts = async (token: string): Promise<void> => {
+        const { status, data }: { status: number; data: ApiResponse } =
+            await axiosInstance.get("friends/feed", {
+                headers: {
+                    token: token, // Send token in headers
+                },
+            });
+        if (status !== 200) {
+            alert("Error getting posts");
+            return;
+        }
+        setPosts(() => data.data.data); // Set posts state
+    };
+
+    useEffect(() => {
+        const cookies = new Cookies(); // Cookies instance
+        const token = cookies.get("token"); // Get token from cookies
+        getPosts(token);
+    }, []);
+
+    return (
+        <div>
+            {posts.map((post: Post) => {
+                return <PostElement post={post} key={post.id} />;
+            })}
+        </div>
+    );
 }
 
 export default PostFeed;
