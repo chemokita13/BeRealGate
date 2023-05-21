1885b0e7e91036f6029d9ceda33102f64f3f3ba1
chemokita13 <chemokita13@gmail.com>
2023-05-21 16:17:22 +0200

    post component finished

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,8 +1,33 @@
 import { Post } from "@/types/types";
 
-function post(Post: Post) {
-    console.log("🚀 ~ file: post.tsx:4 ~ post ~ Post:", Post);
-    return <div>post</div>;
+function PostElement({ post }: { post: Post }) {
+    //?console.log("🚀 ~ file: post.tsx:4 ~ post ~ Post:", Post);
+    return (
+        <div>
+            <h1>{post.userName}</h1>
+            <p>{post.caption}</p>
+            <img
+                src={post.photoURL}
+                width={post.imageWidth / 4}
+                height={post.imageHeight / 4}
+            />
+            <img
+                src={post.secondaryPhotoURL}
+                width={post.secondaryImageWidth / 8}
+                height={post.secondaryImageHeight / 8}
+            />
+            <div>
+                {post.comment.map((comment) => {
+                    return (
+                        <div key={comment.id}>
+                            <h5>{comment.userName}</h5>
+                            <p>{comment.text}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
-export default post;
+export default PostElement;
