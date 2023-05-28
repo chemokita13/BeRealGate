2f57af5a280bf2c425736027b0be2c1f0bcdacc5
chemokita13 <chemokita13@gmail.com>
2023-05-28 14:39:00 +0200

    Added Music, RealMogis and CommentBox

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,40 +1,15 @@
 import axiosInstance from "@/constants/axiosInstance";
-import { ApiResponse, CommentsEntity, Post } from "@/types/types";
+import {
+    ApiResponse,
+    CommentsEntity,
+    Post,
+    RealMojisEntity,
+} from "@/types/types";
 import { useState } from "react";
 import Cookies from "universal-cookie";
+import CommentsBox from "./commentsBox";
 
 function PostElement({ post }: { post: Post }) {
-    const cookies = new Cookies(); // Cookies instance
-    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const token = cookies.get("token"); // Get new token
-        const { status, data }: { status: number; data: ApiResponse } =
-            await axiosInstance.post(
-                "post/comment",
-                {
-                    comment: comment,
-                    postId: postInstance.id,
-                },
-                {
-                    headers: {
-                        token: token,
-                    },
-                }
-            );
-        if (status !== 201) {
-            alert("Something went wrong");
-            return;
-        }
-        const newComent: CommentsEntity = data.data;
-
-        setPostInstance(() => {
-            return {
-                ...postInstance,
-                comments: [...postInstance.comments, newComent],
-            };
-        });
-    };
-    const [comment, setComment] = useState<string>(""); // comment state
     const [postInstance, setPostInstance] = useState<Post>(post); // post state for updating it
     return (
         <div>
@@ -49,25 +24,38 @@ function PostElement({ post }: { post: Post }) {
                 width={postInstance.secondary.width / 8}
                 height={postInstance.secondary.height / 8}
             />
+
+            <CommentsBox
+                postInstance={postInstance}
+                setPostInstance={setPostInstance}
+            />
+
             <div>
-                {postInstance.comments.map((comment: CommentsEntity) => {
+                {postInstance.realMojis.map((realMoji: RealMojisEntity) => {
                     return (
-                        <div key={comment.id}>
-                            <h5>{comment.user.username}</h5>
-                            <p>{comment.content}</p>
+                        <div key={realMoji.id}>
+                            <img
+                                src={realMoji.media.url}
+                                width={realMoji.media.width / 10}
+                                height={realMoji.media.height / 10}
+                            />
+                            <h3>
+                                {realMoji.user.username} - {realMoji.type}
+                            </h3>
                         </div>
                     );
                 })}
-                <form onSubmit={(e) => handleFormSubmit(e)}>
-                    <input
-                        type="text"
-                        name="comment"
-                        id="comment"
-                        onChange={(e) => setComment(e.currentTarget.value)}
-                    />
-                    <button type="submit">Comment</button>
-                </form>
             </div>
+            {postInstance.music && (
+                <div>
+                    <h3>
+                        <a href={postInstance.music.openUrl}>
+                            {postInstance.music.track} -
+                            {postInstance.music.artist}
+                        </a>
+                    </h3>
+                </div>
+            )}
         </div>
     );
 }
