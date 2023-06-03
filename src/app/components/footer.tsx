bf9be5d8fd07d71770351e04d80e5012f04cde7e
chemokita13 <chemokita13@gmail.com>
2023-06-03 11:33:43 +0200

    footer styles

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,9 +2,22 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer>
-            Footer
-            <a href="#top">Scroll to top</a>
+        <footer className="relative bottom-0 flex flex-col items-center text-white bg-black border-t-2 border-double h-36 border-t-white">
+            <a
+                href="#top"
+                className="underline before:content-['⏫'] after:content-['⏫'] m-1 hover:scale-110 transition-all duration-200"
+            >
+                Scroll to top
+            </a>
+            <div className="flex flex-col items-center p-1 m-5 transition-all duration-200 border border-white border-double hover:scale-110">
+                <img
+                    src="https://logodix.com/logo/952890.png"
+                    alt="GitHub source code"
+                    width={50}
+                    height={50}
+                />
+                <span>View source code</span>
+            </div>
         </footer>
     );
 }
