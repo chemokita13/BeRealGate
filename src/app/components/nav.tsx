5398e0d236117ac8bb2cec3183e2758aa035ce92
chemokita13 <chemokita13@gmail.com>
2023-06-03 12:31:59 +0200

    index link

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -13,9 +13,11 @@ function Nav() {
             >
                 Login
             </Link>
-            <h1 className="text-3xl font-extrabold transition-all cursor-pointer md:text-8xl sm:text-4xl hover:scale-105 hover:underline">
-                BeReal Gate
-            </h1>
+            <Link href="/">
+                <h1 className="text-3xl font-extrabold transition-all cursor-pointer md:text-8xl sm:text-4xl hover:scale-105 hover:underline">
+                    BeReal Gate
+                </h1>
+            </Link>
             <Link
                 href="/posts"
                 className="p-3 m-1 font-semibold underline transition-all duration-200 border border-white rounded-lg sm:p-5 sm:m-10 hover:scale-105 hover:text-xl"
