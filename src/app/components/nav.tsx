a3924f57ca64abfcfbeae9598ccd98838e827a84
chemokita13 <chemokita13@gmail.com>
2023-06-03 12:29:13 +0200

    responsive style

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -3,17 +3,22 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav className="flex items-center justify-between h-16 text-white bg-black sm:flex-row sm:h-36" id="top">  
+        <nav
+            className="flex items-center justify-between h-16 text-white bg-black drop-shadow-2xl sm:flex-row sm:h-36"
+            id="top"
+        >
             <Link
                 href="/login"
-                className="p-3 m-1 font-semibold underline border border-white rounded-lg sm:p-5 sm:m-10"
+                className="p-3 m-1 font-semibold underline transition-all duration-200 border border-white rounded-lg sm:p-5 sm:m-10 hover:scale-105 hover:text-xl"
             >
                 Login
             </Link>
-            <h1 className="text-3xl font-extrabold sm:text-8xl">BeReal Gate</h1>
+            <h1 className="text-3xl font-extrabold transition-all cursor-pointer md:text-8xl sm:text-4xl hover:scale-105 hover:underline">
+                BeReal Gate
+            </h1>
             <Link
                 href="/posts"
-                className="p-3 m-1 font-semibold underline border border-white rounded-lg sm:p-5 sm:m-10"
+                className="p-3 m-1 font-semibold underline transition-all duration-200 border border-white rounded-lg sm:p-5 sm:m-10 hover:scale-105 hover:text-xl"
             >
                 Posts
             </Link>
