fa9aab0ad5985a6f9308a7d919b93eb327e864ec
chemokita13 <chemokita13@gmail.com>
2023-06-03 12:29:04 +0200

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
 import Layout from "@/app/layout";
-import RootLayout from "@/app/layout";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <Layout>
-            <main>
-                <h1> Hello world!"</h1>
+            <main
+                id="main-index"
+                className="flex flex-col justify-center text-left text-white align-middle"
+            >
+                <h1 className="p-1 text-5xl font-extrabold cursor-pointer ml-7 sm:ml-16 ">
+                    A new way to BeReal.
+                </h1>
+                <h2 className="ml-10 text-2xl font-semibold cursor-pointer sm:ml-20">
+                    Say "Hello!" to <u>BeReal Gate</u>
+                </h2>
+                <div className="flex flex-row items-center gap-5 m-5 mb-56 ml-14 sm:ml-24">
+                    <Link
+                        href="/login"
+                        className="px-5 py-3 text-xl transition-all duration-150 border border-white rounded-lg hover:underline hover:scale-110"
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        href="/posts"
+                        className="px-5 py-3 text-xl transition-all duration-150 border border-white rounded-lg hover:underline hover:scale-110"
+                    >
+                        Posts
+                    </Link>
+                    <a
+                        href=""
+                        className="px-5 py-3 text-xl transition-all duration-150 border border-white rounded-lg hover:underline hover:scale-110"
+                    >
+                        GitHub
+                    </a>
+                </div>
             </main>
         </Layout>
     );
