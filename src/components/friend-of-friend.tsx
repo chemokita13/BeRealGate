69d9e46ffa54a92e493d9ab00e366371e7c4c573
José María Pahino <chemokita13@gmail.com>
2023-08-27 22:41:49 +0200

    added friends in fof posts

--- src/components/friend-of-friend.tsx
+++ src/components/friend-of-friend.tsx
@@ -13,22 +13,42 @@ function FriendOfFriendPosts({
     return (
         <div className="m-1 border border-white sm:flex sm:flex-col sm:w-[30%] sm:items-center rounded-md">
             {FriendOfFriend.user.profilePicture ? (
-                <div className="flex flex-row items-center gap-3 p-3">
-                    <img
-                        src={FriendOfFriend.user.profilePicture.url}
-                        className="rounded-full"
-                        width={50}
-                        height={50}
-                        alt="Profile picture"
-                    />
-                    <h1 className="text-xl font-bold text-center underline sm:font-bold before:content-['@'] before:font-extrabold">
+                <>
+                    <div className="flex flex-row items-center gap-3 p-3">
+                        <img
+                            src={FriendOfFriend.user.profilePicture.url}
+                            className="rounded-full"
+                            width={50}
+                            height={50}
+                            alt="Profile picture"
+                        />
+                        <h1 className="text-xl font-bold text-center underline sm:font-bold before:content-['@'] before:font-extrabold">
+                            {FriendOfFriend.user.username}
+                        </h1>
+                    </div>
+                    <h2 className="text-sm text-center text-gray-400">
+                        Friend of:{" "}
+                        {FriendOfFriend.user.relationship.commonFriends.map(
+                            (friend) => {
+                                return " " + friend.username + ",";
+                            }
+                        )}
+                    </h2>
+                </>
+            ) : (
+                <>
+                    <h1 className="text-xl p-3 font-bold text-center underline sm:font-bold before:content-['@'] before:font-extrabold">
                         {FriendOfFriend.user.username}
                     </h1>
-                </div>
-            ) : (
-                <h1 className="text-xl p-3 font-bold text-center underline sm:font-bold before:content-['@'] before:font-extrabold">
-                    {FriendOfFriend.user.username}
-                </h1>
+                    <h2 className="text-sm text-center text-gray-400">
+                        Friend of:{" "}
+                        {FriendOfFriend.user.relationship.commonFriends.map(
+                            (friend) => {
+                                return " " + friend.username + ",";
+                            }
+                        )}
+                    </h2>
+                </>
             )}
             <div className="flex flex-row overflow-hidden min-w-[300px] w-[375px] carousel m-auto sm:m-0">
                 <PostElement
