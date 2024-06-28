58782a1a05b985c167733fe3fb49925e25091bfa
José María Pahino <chemokita13@gmail.com>
2024-06-28 17:04:54 +0200

    changed bodyparser

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
     env: {
         API_URL: process.env.API_URL,
-    }
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "12mb", // Set desired value here
+        },
+    },
 };
 
 module.exports = nextConfig;
